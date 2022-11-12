3a774a099cad5320798372f4fe0000b0cc7b0ba9
zolanovich <avilajuan0301@gmail.com>
2022-11-12 17:41:20 -0400

    Clase 12/11/2022 Clase BaseVisitor

---
+++ ParseadorArimetica/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+
+namespace ParseadorArimetica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = "5+6+9+3-1";
+            ICharStream stream = CharStreams.fromString(input);
+            arimeticaLexer lexer = new arimeticaLexer(stream);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            arimeticaParser parser = new arimeticaParser(tokens);
+            arimeticaParser.ArimeticaContext tree = parser.arimetica();
+            arimetica.Arimetica arimetica = new arimetica.Arimetica();
+            int res = arimetica.Visit(tree);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ParseadorArimetica/arimetica.cs
@@ -0,0 +1,46 @@
+﻿using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseadorArimetica
+{
+    class arimetica
+    {
+        internal class Arimetica : arimeticaBaseVisitor<int>
+        {
+            public override int Visit(IParseTree tree)
+            {
+                return base.Visit(tree);
+            }
+
+            public override int VisitArimetica([NotNull] arimeticaParser.ArimeticaContext context)
+            {
+                Console.WriteLine(context.GetText());
+                return base.VisitArimetica(context);
+            }
+
+            public override int VisitExpresion([NotNull] arimeticaParser.ExpresionContext context)
+            {
+                Console.WriteLine(context.GetText());
+                return base.VisitExpresion(context);
+            }
+
+            public override int VisitFactor([NotNull] arimeticaParser.FactorContext context)
+            {
+                Console.WriteLine(context.GetText());
+                return base.VisitFactor(context);
+            }
+
+
+            public override int VisitTermino([NotNull] arimeticaParser.TerminoContext context)
+            {
+                Console.WriteLine(context.GetText());
+                return base.VisitTermino(context);
+            }
+        }
+    }
+}
